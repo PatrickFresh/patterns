5aef676a45fb99f7bf6181380d29032baf06bc63
Patryk <dudis9999@gmail.com>
2017-10-17 22:32:05 +0200

    Added Singleton pattern.

---
+++ Patterns/SingletonPattern/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Current time: {DateTime.Now}, {Singleton.Instance.Show()}");
+            System.Threading.Thread.Sleep(5000);
+            Console.WriteLine($"Current time: {DateTime.Now}, {Singleton.Instance.Show()}");
+            System.Threading.Thread.Sleep(5000);
+            Console.WriteLine($"Current time: {DateTime.Now}, {Singleton.Instance.Show()}");
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Patterns/SingletonPattern/Singleton.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    public class Singleton
+    {
+        private static Singleton instance = null;
+        private static object locker = new object();
+        private DateTime creationDate;
+        private Singleton()
+        {
+            creationDate = DateTime.Now;
+        }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                lock (locker)
+                {
+                    return instance ?? (instance = new Singleton());
+                }
+            }
+        }
+
+        public string Show()
+        {
+            return $"Singleton instance created: {creationDate.ToString()}";
+        }
+    }
+}
